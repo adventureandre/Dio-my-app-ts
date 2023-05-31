8ebf1f9cec90dace4dbf16e5ce9a5f1286b380da
andre <adventureandre@hotmail.com>
2023-05-30 22:56:15 -0300

    Test unitarios

--- src/services/soma.test.tsx
+++ src/services/soma.test.tsx
@@ -7,7 +7,17 @@ describe('soma', ()=>{
     })
 
     it('deve multiplicar o número por 2 ', function () {
-        const value = multiplica(2)
-        expect(value).toBe(2)
+        const value = multiplica(2,2)
+        expect(value).toBe(4)
+    });
+
+    it('deve multiplicar o número por 3 ', function () {
+        const value = multiplica(2,3)
+        expect(value).toBe(6)
+    });
+
+    it('deve informa um erro com multiplicador diferente de 2,3 ', function () {
+        const value = multiplica(2,4)
+        expect(value).toBe('Multipliador não aceito')
     });
 })
--- src/services/soma.tsx
+++ src/services/soma.tsx
@@ -2,6 +2,9 @@ export const soma = (num: number): number => {
     return num + 1;
 }
 
-export const multiplica = (num:number) =>{
-    return num;
+export const multiplica = (num:number,mult:number):number | string  =>{
+    if(mult === 2 || mult === 3){
+        return num*mult;
+    }
+    return "Multipliador não aceito"
 }
