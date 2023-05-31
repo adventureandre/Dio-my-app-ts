3d88b84b11bff533d9003f264cba18c961f474ee
andre <adventureandre@hotmail.com>
2023-05-30 23:07:16 -0300

    Test unitarios

---
+++ src/services/login.test.tsx
@@ -0,0 +1,12 @@
+import {login} from "./login";
+
+describe('login',()=>{
+
+    const mockAlert = jest.fn()
+    window.alert  = mockAlert
+
+    it('Deve Exibir um alert com boas Vindas', function () {
+        login()
+        expect(mockAlert).toBeCalled()
+    });
+})
